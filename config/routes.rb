c0a99d6713b2c791d42fb1d0367826d0d469834e
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-08 23:38:36 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'posts/index'
-  get 'posts/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/id', to: 'posts#show', as: 'user_post'
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/id', to: 'users#show', as: 'user'
+root 'users#index'
 end
