42ec73a08d891dbe29128e6ec3b887e0782a5a61
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-09 00:24:15 +0500

    Make optional change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/id', to: 'posts#show', as: 'user_post'
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/id', to: 'users#show', as: 'user'
+  resources :users, only: [:index, :show] do resources :posts, only: [:index, :show] end
 root 'users#index'
 end
