d5185639996157690ac237313c93137ab398e031
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-08 22:40:53 +0500

    Add files for the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'posts/index'
+  get 'posts/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
