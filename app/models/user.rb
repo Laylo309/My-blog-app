1fb8aabb074e34d44603e881523029cbc1726672
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-15 20:15:14 +0500

    Update users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, :comments, :likes, foreign_key: 'author_id'
+
+  # A method that returns the 3 most recent posts for a given user.
+  def recent_posts 
+    User.order(created_at :asc).limit(3)
+  end
 end
