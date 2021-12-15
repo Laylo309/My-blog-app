95a82245cd101ebbac82f0679e98bccae73274ab
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-15 20:14:33 +0500

    update comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user, :post
+# A method that updates the comments counter for a post.
+  def update_comments_counter(id,comment)
+    Post.find(id).update(comments_counter :comment)
+  end
 end
