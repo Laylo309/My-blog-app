47ededfbd94b167fd9bf79cb7fd4bac657e4582e
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-15 20:14:56 +0500

    update posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, :likes, foreign_key: 'post_id'
+
+  # A method that updates the posts counter for a user.
+  def update_posts_counter(id,post)
+    User.find(id).update(posts_counter :post)
+  end
+    # A method that returns the 5 most recent posts for a given post.
+    def comments 
+    User.order(created_at :asc).limit(5)
+  end
 end
