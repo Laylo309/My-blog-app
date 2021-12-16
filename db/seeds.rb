c2bf5d27b415ddbf682309d9b4215483e21ee15b
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-16 18:00:52 +0500

    Add database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  { name: 'Laylo', photo: 'Cool_Photo_1', bio: "I'm the sleepy oracle", posts_counter: 0},
+  { name: 'Nelsino', photo: 'Cool_Photo_1', bio: "I'm the last oracle", posts_counter: 0},
+  { name: 'Hamza', photo: 'Cool_Photo_1', bio: "I'm the First oracle", posts_counter: 0},
+  { name: 'Juan', photo: 'Cool_Photo_1', bio: "I'm the Best oracle", posts_counter: 0},
+  { name: 'Mateo', photo: 'Cool_Photo_1', bio: "I'm the Crazy oracle", posts_counter: 0},
+  { name: 'Herbert', photo: 'Cool_Photo_1', bio: "I'm the rider oracle", posts_counter: 0}
+  ])
+
+all_users = User.all
+
+all_users.each do |user|
+  for j in 1..5 do
+    Post.create(title: "Post number: #{j}", text: "I'm writing my post number: #{j}", comments_counter: 0, likes_counter: 0, author_id: user.id)
+  end
+end
+
+all_posts = Post.all
+
+all_posts.each do |post|
+  for j in 1..5 do
+    Like.create(author_id: all_users[j].id, post_id: post.id)
+    Comment.create(author_id: all_users[j].id, post_id: post.id, text: "I'm #{all_users[j].name} and I'm commenting gibberish here.")
+  end
+end
