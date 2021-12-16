89202345c1dd144fb8902bd9edfece021a792410
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-17 00:20:58 +0500

    Add users info

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def get_img(user)
-    user.photo.present? ? user.photo: 'https://eu.ui-avatars.com/api/?name=#{user.name}'
+  "https://eu.ui-avatars.com/api/?name=#{user.name}"
   end
 end
