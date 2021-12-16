57f86d4201f6436ac863426cb8ee235c5caf09ab
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-16 21:34:27 +0500

    Setup controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+  @user = User.find(params[:user_id])
+   @posts = @user.recent_posts
+  end
 
-  def show; end
+  def show
+  @post = @user.posts.find(params[:id])
+   @user = User.find(params[:user_id])
+   @comments = @post.comments.all
+  end
 end
