55bc85cc0bf9f55e4ae5a7ce40a091d1114dc893
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-18 03:16:45 +0500

    Update publish posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,25 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
     @comments = @post.comments.all
   end
+
+  def new
+    @post = Post.new
+    @current_user  = self.current_user
+  end
+  
+  def create
+    @post = Post.new(params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter))
+
+    respond_to do |format|
+      format.html do
+        redirect_to user_post_path(@post.user.id, @post.id) if @post.save
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
