842eda99ce7052c03c7cfc6034418691385bc924
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-17 05:04:28 +0500

    Add styles

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post
-
   after_save :update_likes_count
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ all_users = User.all
 
 all_users.each do |user|
   for j in 1..5 do
-    Post.create(title: "Post number: #{j}", text: "I'm writing my post number: #{j}", comments_counter: 0, likes_counter: 0, author_id: user.id)
+    Post.create(title: "Post number: #{j}", text: "I'm writing my post number: #{j}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", comments_counter: 0, likes_counter: 0, author_id: user.id)
   end
 end
 
