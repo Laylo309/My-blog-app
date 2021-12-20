bf0a242696ad3bbfb6269c136eaeac25dd975f95
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-20 21:01:09 +0500

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   belongs_to :post
 
-  after_save :increment_counter
+  after_save :update_comments_counter
 
-  def increment_counter
-    post.update(comments_counter: post.comments.count)
+  def update_comments_counter
+   post.increment!(:comments_counter)
   end
 end
