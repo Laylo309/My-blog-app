ffb26ca7b2ca82f8557711563a2babe1c912559c
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-22 03:14:16 +0500

    Add styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.recent_posts
+    @current_user = current_user
   end
 
   def create
