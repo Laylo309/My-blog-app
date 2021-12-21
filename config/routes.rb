5b3f6d53b8cf6a12cbb0960d2a3bc9c4f8ef6fc3
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-21 21:37:59 +0500

    Setup and install devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show] do resources :posts, only: [:index, :show, :new] end
 
     resources :posts, only: [:new, :create] do
