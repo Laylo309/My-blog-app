9fbd1bbc7936426f2dea783cb7ca56ffa7b4ac71
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-23 21:13:09 +0500

    Linter check 1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
     if user.is? :admin
       can :manage, :all
     else
-      can :manage, Post, author_id: user.id 
+      can :manage, Post, author_id: user.id
       can :manage, Comment, author_id: user.id
       can :read, :all
     end
