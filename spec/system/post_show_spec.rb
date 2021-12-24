2d87664ecad731e8302a287c236424a1fee8b967
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-24 23:38:05 +0500

    Add test to post show file

---
+++ spec/system/post_show_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+require './spec/mocks_module'
+
+RSpec.describe 'Post show page', type: :feature do
+  include Mocks
+
+  before :each do
+    users = create_users
+    posts = create_posts(users)
+    visit new_user_session_path
+
+    within('form') do
+      fill_in 'Email', with: 'jgonzalez.wj@gmail.com'
+      fill_in 'Password', with: '123456'
+    end
+    click_button 'Log in'
+  end
+
+  context 'displaying post show page correctly' do
+    it 'Should show posts title' do
+      users = User.all
+
+      users.each do |user|
+        visit user_post_path(user.id)
+        posts = user.posts.all
+        posts.each do |post|
+          expect(page).to have_content post.title
+          visit root_path
+        end
+      end
+    end
+
+    it 'can see who wrote the post' do
+      users = User.all
+
+      users.each do |user|
+        visit user_post_path(user.id)
+        expect(page).to have_content user.name
+        visit root_path
+      end
+    end
+
+    it 'Should show how many comments a post has' do
+      users = User.all
+      
+      users.each do |user|
+        visit user_post_path(user.id)
+        recent_posts = user.recent_posts
+        recent_posts.each do |post|
+          expect(page).to have_content post.comments_counter
+        end
+        visit root_path
+      end
+    end 
+    
+    it 'Should show how many likes a post has' do
+      users = User.all
+      
+      users.each do |user|
+        visit user_post_path(user.id)
+        recent_posts = user.recent_posts
+        recent_posts.each do |post|
+          expect(page).to have_content post.likes_counter
+        end
+        visit root_path
+      end
+    end
+
+    it 'Should show the posts body' do
+      users = User.all
+
+      users.each do |user|
+        visit user_post_path(user.id)
+        posts = user.posts.all
+        posts.each do |post|
+          expect(page).to have_content post.text
+          visit root_path
+        end
+      end
+    end
+
+    it 'can see the username of each commentor' do
+      users = User.all
+
+      users.each do |user|
+        visit user_post_path(user.id)
+        recent_posts = user.recent_posts
+        recent_posts.each do |post|
+          recent_comments = post.recent_comments
+          recent_comments.each do |comment|
+            expect(page).to have_content "#{comment.user.name}"
+          end
+        end
+        visit root_path
+      end
+    end 
+       
+    it 'can see the comment each commentor left' do
+      users = User.all
+
+      users.each do |user|
+        visit user_post_path(user.id)
+        recent_posts = user.recent_posts
+        recent_posts.each do |post|
+          recent_comments = post.recent_comments
+          recent_comments.each do |comment|
+            expect(page).to have_content "#{comment.text}"
+          end
+        end
+        visit root_path
+      end
+    end
+  end
+end
