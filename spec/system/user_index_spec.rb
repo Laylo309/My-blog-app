92c9b9c905d742c884ff919026ff21783edb633d
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-24 09:31:05 -0600

    Fix before each for tests

--- spec/system/user_index_spec.rb
+++ spec/system/user_index_spec.rb
@@ -4,27 +4,56 @@ require './spec/mocks_module'
 
 RSpec.describe 'the user index page', type: :feature do
   include Mocks
-  context 'It should show the name of all users' do
-    before :each do
-      users = create_users
-      
-      visit new_user_session_path
+  before :each do
+    users = create_users
+    
+    visit new_user_session_path
 
-      within('form') do
-        fill_in 'Email', with: 'jgonzalez.wj@gmail.com'
-        fill_in 'Password', with: '123456'
-      end
-      click_button 'Log in'
+    within('form') do
+      fill_in 'Email', with: 'jgonzalez.wj@gmail.com'
+      fill_in 'Password', with: '123456'
     end
+    click_button 'Log in'
+  end
 
+  context 'It should show the name of all users' do
     it 'Should list all users' do
       users = User.all
 
       users.each do |user|
         expect(page).to have_content user.name
-        
       end
-      expect(page).to have_content 'juanito'
+    end
+
+    it 'can see all users photos' do
+      users = User.all.order(:id)
+      imgs = page.all('img')
+
+      users.each_with_index do |user, index|
+        expect(imgs[index]['src']).to include(user.name)
+      end
+      expect(imgs.length).to be(users.length)
+    end
+
+    it 'Should list all users post counter' do
+      users = User.all
+
+      users.each do |user|
+        expect(page).to have_content user.posts_counter
+      end
+    end
+  end
+
+  context 'is redirect to user page show' do
+    it 'compares user path with current path' do
+      users = User.all
+
+      users.each do |user|
+        visit root_path
+        click_link user.name
+        expect(current_path).to eq(user_path(user.id))
+        visit root_path
+      end
     end
   end
 end
