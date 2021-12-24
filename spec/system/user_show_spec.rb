1e6e0b9d09d033e0e129c7992a410750f597bdc7
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-24 09:28:45 -0600

    Add tests for user show page

---
+++ spec/system/user_show_spec.rb
@@ -0,0 +1,116 @@
+require 'rails_helper'
+
+require './spec/mocks_module'
+
+RSpec.describe 'the user index page', type: :feature do
+  include Mocks
+
+  before :each do
+    users = create_users
+    posts = create_posts(users)
+    visit new_user_session_path
+
+    within('form') do
+      fill_in 'Email', with: 'jgonzalez.wj@gmail.com'
+      fill_in 'Password', with: '123456'
+    end
+    click_button 'Log in'
+  end
+
+  context 'displaying user show page correctly' do
+    it 'can see user picture' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+        img = page.find('img')
+        expect(img['src']).to include(user.name)
+        visit root_path
+      end
+    end
+
+    it 'Should list user`s name' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+        expect(page).to have_content user.name
+        visit root_path
+      end
+    end
+
+    it 'Should list all users post counter' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+        expect(page).to have_content user.posts_counter
+        visit root_path
+      end
+    end
+
+    it 'Should list all users bio' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+        expect(page).to have_content user.bio
+        visit root_path
+      end
+    end
+
+    it 'can see user first 3 post' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+        recent_posts = user.recent_posts
+        expect(recent_posts.length).to eq(3)
+        visit root_path
+      end
+    end
+
+    it 'can see user recent_posts' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+        recent_posts = user.recent_posts
+        recent_posts.each do |post|
+          expect(page).to have_content "#{post.text}"
+        end
+
+        visit root_path
+      end
+    end
+
+    it 'can see button to show all posts' do
+      users = User.all
+
+      users.each do |user|
+        visit user_path(user.id)
+          expect(page).to have_content "See all posts"
+        visit root_path
+      end
+    end
+
+  end
+
+  context 'is redirect to user post page index' do
+    it 'compares user path with current path' do
+      users = User.all
+
+      users.each do |user|
+        click_link user.name
+        recent_posts = user.recent_posts
+        recent_posts.each do |post|
+          click_link post.name
+          expect(current_path).to eq(user_post_path(user.id, post.id))
+        end
+        visit root_path
+      end
+    end
+  end
+end
+
+
